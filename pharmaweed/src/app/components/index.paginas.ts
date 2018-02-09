bbcd868089c260c203d556ac18e64969e8398dad
pablo@rhodium.cl <pablo@rhodium.cl>
2018-02-09 17:41:41 -0300

    rutas

---
+++ pharmaweed/src/app/components/index.paginas.ts
@@ -0,0 +1,3 @@
+export { AboutComponent } from "./about/about.component";
+export { PharmaweedComponent } from "./pharmaweed/pharmaweed.component";
+export { ItemComponent } from "./item/item.component";
